f6637b28dc698cd5ef0085baf317224271d6589b
Martins Polakovs <martins.polakovs@gmail.com>
2013-12-28 15:39:15 +0200

    Initial commit, empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ElFinderApp::Application.config.secret_key_base = '4daa92ab3e42376486c18df255f8171cd9e76080875c650514688ca02f7abd04aa07311c5fd535ad5a5d6c066be21153d986053d817935ca2329220bae609618'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ElFinderApp::Application.config.session_store :cookie_store, key: '_el_finder_app_session'
